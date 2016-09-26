cfee1fb38053e41178e0ace2a063d6a4920118f2
Dhruv Parmar <dparmar@mediamath.com>
2016-09-26 16:34:32 -0400

    Added load all functionality for easy character loading

--- test_bot.py
+++ test_bot.py
@@ -171,24 +171,45 @@ async def choose(*choices : str):
 
 
 @bot.command()
-async def load(char_name):
+async def load(char_name : str):
 	"""Load specified character"""
 	data = []
+	chars_to_load = []
 	exists = False
-	fo = open(char_name + ".txt", "r")
-	data = [line.rstrip('\n') for line in fo]
-	fo.close()
-	for i,v in enumerate(data[:]):
-		tok = v.find('=')
-		data[i] = v[tok+1:]
-	for char in party:
-		if (data[0] == char.name):
-			await bot.say('```%s already exists in the party.```' %char_name)
-			exists = True
-	if (exists == False):
-		temp = character(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22])
-		party.append(temp)
-		await bot.say('```%s has been added to the party!```' %data[0])
+	if (char_name == "all"):
+		fo = open("party.txt", "r")
+		chars_to_load = [line.rstrip('\n') for line in fo]
+		fo.close()
+		for char in chars_to_load:
+			fo = open(char + ".txt", "r")
+			data = [line.rstrip('\n') for line in fo]
+			fo.close()
+			for i,v in enumerate(data[:]):
+				tok = v.find('=')
+				data[i] = v[tok+1:]
+			for char in party:
+				if (data[0] == char.name):
+					await bot.say('```%s already exists in the party.\n```' %char)
+					exists = True
+			if (exists == False):
+				temp = character(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22])
+				party.append(temp)
+				await bot.say('```%s has been added to the party!\n```' %data[0])
+	else:
+		fo = open(char_name + ".txt", "r")
+		data = [line.rstrip('\n') for line in fo]
+		fo.close()
+		for i,v in enumerate(data[:]):
+			tok = v.find('=')
+			data[i] = v[tok+1:]
+		for char in party:
+			if (data[0] == char.name):
+				await bot.say('```%s already exists in the party.\n```' %char_name)
+				exists = True
+		if (exists == False):
+			temp = character(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22])
+			party.append(temp)
+			await bot.say('```%s has been added to the party!\n```' %data[0])
 
 @bot.command()
 async def display(char_name : str, stat : str = None):
